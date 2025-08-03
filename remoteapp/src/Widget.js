650f83a5d373aa64b1c0b210219f9f58d7bed33f
balajin96 <balajinatarajan18011996@gmail.com>
2025-08-04 00:40:41 +0530

    2nd commit

--- remoteapp/src/Widget.js
+++ remoteapp/src/Widget.js
@@ -6,7 +6,7 @@ const Widget = () => {
     <div style={{ padding: "20px", border: "2px dashed #666", backgroundColor: "#000", borderRadius: "8px" }}>
       <h2>Remote Widget</h2>
       <p>This component is from the <strong>remote-app</strong>!</p>
-      <p>Successfully loaded via Module Federation! </p>
+      <p>Successfully loaded via Module Federation! 2 PUSH CODE</p>
     </div>
   );
 };
